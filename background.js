8cd1e6512ac55e7f4be55800574a0cd152699cb6
Mohammed Abdul Aziz Syed <msyed@godaddy.com>
2020-11-23 16:30:45 -0600

    making plugin kick in on page load

--- background.js
+++ background.js
@@ -1,9 +1,11 @@
-chrome.tabs.onActivated.addListener(tab => {
-    chrome.tabs.get(tab.tabId, current_tab_info => {
-        if (/^https:\/\/account\.godaddy\.com/.test(current_tab_info.url)) {
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status == 'complete' && tab.active) {
+        if (/^https:\/\/account\.godaddy\.com/.test(tab.url)) {
             chrome.tabs.executeScript(null, { file: './foreground.js' }, () => {
                 console.log('foreground script injected');
             });
         }
-    });
+    }
+    // chrome.tabs.get(tab.tabId, current_tab_info => {
+    // });
 });
