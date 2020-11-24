10ab7b31f91dbb4040e3bfee744b34a116710d0e
akerr <akerr@lmtc-adkerr.jomax.paholdings.com>
2020-11-24 11:55:58 -0800

    add panel.js

---
+++ src/panel.js
@@ -0,0 +1,13 @@
+chrome.devtools.network.onRequestFinished.addListener(
+  function(request) {
+      if (request &&
+          request.request &&
+          request.request.url &&
+          request.request.url.startsWith('https://events.api.godaddy.com/pageEvents.aspx')) {
+          const trafficData = request.request.url.replace('https://events.api.godaddy.com/pageEvents.aspx?', '');
+          const trafficObject = JSON.parse('{"' + decodeURI(trafficData).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}');
+          console.log('timestamp', trafficObject.timestamp);
+          console.log('e_id', trafficObject.e_id);
+      }
+  }
+);
